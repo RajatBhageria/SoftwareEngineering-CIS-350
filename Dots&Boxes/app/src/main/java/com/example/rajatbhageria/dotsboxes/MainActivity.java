1afb322a1fd46a8f3310cfa0adbafb92b5216cce
Rajat Bhageria <rajat1996@gmail.com>
2016-02-06 18:24:20 -0500

    Trying again

--- Dots&Boxes/app/src/main/java/com/example/rajatbhageria/dotsboxes/MainActivity.java
+++ Dots&Boxes/app/src/main/java/com/example/rajatbhageria/dotsboxes/MainActivity.java
@@ -1,22 +1,23 @@
-package com.example.rajatbhageria.dotsandboxes;
+package com.example.rajatbhageria.dotsboxes;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
     @Override
@@ -46,9 +47,9 @@ public class MainActivity extends ActionBarActivity {
         // Create an ArrayAdapter using the string array and a default spinner layoutw
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.spinner_array, android.R.layout.simple_spinner_item);
-                // Specify the layout to use when the list of choices appears
+        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            // Apply the adapter to the spinner
+        // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
     }
 
@@ -62,8 +63,4 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public static final int GameActivity_ID = 1;
-
-
 }
-
-
